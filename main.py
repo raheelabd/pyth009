fc1c1a592f7d21ab30178172cdb3bc4d2685fc9d
raheelabd <138661939+raheelabd@users.noreply.github.com>
2023-07-12 00:58:53 +0300

    solve h.w for today

---
+++ main.py
@@ -0,0 +1,15 @@
+n=4
+if n % 2==0:
+    print("number is even")
+else:
+    print("number is odd")
+n=10
+i=2
+if n >1:
+    if n%i==0:
+        print("number is prime")
+    else:
+        print("number is not prime")
+
+
+
